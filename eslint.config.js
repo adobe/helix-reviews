3face8c101a1d67cf5f84dd3c5fb99bc13611450
Tobias Bocanegra <tripod@adobe.com>
2026-08-27 11:19:37 +0200

    chore: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -9,21 +9,24 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+import { defineConfig, globalIgnores } from '@eslint/config-helpers';
+import { recommended, source, test } from '@adobe/eslint-config-helix';
 
-module.exports = {
-  root: true,
-  extends: '@adobe/eslint-config-helix',
-  env: {
-    node: false,
-    browser: true,
+export default defineConfig([
+  globalIgnores([
+    '.vscode/*',
+    'coverage/*',
+    'dist/*',
+  ]),
+  {
+    rules: {
+      'import/no-unresolved': ['error', { ignore: ['@octokit/rest', 'file-type'] }],
+    },
+    plugins: {
+      import: recommended.plugins.import,
+    },
+    extends: [recommended],
   },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2022,
-  },
-  rules: {
-    'import/extensions': ['error', {
-      js: 'always',
-    }],
-  },
-};
+  source,
+  test,
+]);
